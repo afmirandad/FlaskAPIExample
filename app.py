6f774b20f9b73ea54bade704b012567faf6d6c84
AndresMiranda <61157532+afmirandad@users.noreply.github.com>
2025-09-10 23:49:51 +0000

    Railway connection

--- app.py
+++ app.py
@@ -29,7 +29,6 @@ app.register_blueprint(user_bp)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Crea las tablas si no existen
-    app.run(debug=True)
 
 """
 Para agregar más controladores, importa y registra el blueprint correspondiente:
